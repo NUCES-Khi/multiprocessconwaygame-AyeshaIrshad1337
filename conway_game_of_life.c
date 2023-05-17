e16d9b0f5f4b85c34451fc5b73c42141f2b09d81
Ayesha Irshad <104616632+AyeshaIrshad1337@users.noreply.github.com>
2023-05-17 14:04:48 +0500

    Update conway_game_of_life.c

--- conway_game_of_life.c
+++ conway_game_of_life.c
@@ -11,123 +11,147 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <sys/shm.h>
 #include <sys/wait.h>
-#include <string.h>
 
-// Function to print the grid
-void printGeneration(int *grid, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%d ", grid[i * n + j]);
+#define BOARD_SIZE 5
+#define NUM_GENERATIONS 5
+#define NUM_PROCESSES 4
+
+// Function to initialize the game board with random values
+void init_board(int *board) {
+    for (int i = 0; i < BOARD_SIZE * BOARD_SIZE; i++) {
+        board[i] = rand() % 2;
+    }
+}
+
+// Function to print the game board
+void print_board(int *board) {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            printf("%d ", board[i * BOARD_SIZE + j]);
         }
         printf("\n");
     }
+    printf("\n");
 }
 
-// Function to update the generation for a single row
-void updateRow(int *grid, int *newGrid, int row, int n) {
-    int neighbors;
-    for (int col = 0; col < n; col++) {
-        neighbors = 0;
-        // Count number of alive neighbors
-        for (int r = row - 1; r <= row + 1; r++) {
-            for (int c = col - 1; c <= col + 1; c++) {
-                if (r >= 0 && r < n && c >= 0 && c < n && !(r == row && c == col) && grid[r * n + c] == 1) {
-                    neighbors++;
-                }
+// Function to apply the rules of the game to a single cell
+int apply_rules(int *board, int x, int y) {
+    int num_neighbors = 0;
+    for (int i = x - 1; i <= x + 1; i++) {
+        for (int j = y - 1; j <= y + 1; j++) {
+            if (i == x && j == y) {
+                continue;
+            }
+            if (i < 0 || i >= BOARD_SIZE || j < 0 || j >= BOARD_SIZE) {
+                continue;
+            }
+            num_neighbors += board[i * BOARD_SIZE + j];
+        }
+    }
+    if (board[x * BOARD_SIZE + y] == 1) {
+        if (num_neighbors == 2 || num_neighbors == 3) {
+            return 1;
+        } else {
+            return 0;
+        }
+    } else {
+        if (num_neighbors == 3) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+// Function to simulate a portion of the game board
+void simulate_board(int *board, int start_row, int end_row, int *pipe_fd) {
+    int msg = 0;
+    for (int gen = 0; gen < NUM_GENERATIONS; gen++) {
+        // Apply the rules of the game to each cell in the sub-grid
+        for (int i = start_row; i < end_row; i++) {
+            for (int j = 0; j < BOARD_SIZE; j++) {
+                int new_val = apply_rules(board, i, j);
+                board[i * BOARD_SIZE + j] = new_val;
             }
         }
 
-        // Update cell state based on neighbors
-        if (grid[row * n + col] == 1) {
-            if (neighbors == 2 || neighbors == 3) {
-                newGrid[row * n + col] = 1;
-            } else {
-                newGrid[row * n + col] = 0;
-            }
-        } else {
-            if (neighbors == 3) {
-                newGrid[row * n + col] = 1;
-            } else {
-                newGrid[row * n + col] = 0;
-            }
-        }
+        // Send a message to the parent process indicating that this process has completed one iteration
+        write(pipe_fd[1], &msg, sizeof(msg));
+
+        // Wait for a message from the parent process to proceed to the next iteration
+        read(pipe_fd[0], &msg, sizeof(msg));
     }
+
+    // Send a message to the parent process indicating that this process has completed all iterations
+    write(pipe_fd[1], &msg, sizeof(msg));
 }
 
 int main() {
-    int n;
-    printf("Enter the size of the grid (n x n): ");
-    scanf("%d", &n);
+    int *board;
+    int board_size = BOARD_SIZE * BOARD_SIZE;
+    int shmid = shmget(IPC_PRIVATE, board_size * sizeof(int), IPC_CREAT | 0666);
+    board = (int *) shmat(shmid, NULL, 0);
+    init_board(board);
+    int pipe_fd[NUM_PROCESSES][2];
+    pid_t pid[NUM_PROCESSES];
+    int rows_per_process = BOARD_SIZE / NUM_PROCESSES;
 
-    // Allocate shared memory for the grid
-    int *grid = (int *)malloc(n * n * sizeof(int));
-    int *newGrid = (int *)malloc(n * n * sizeof(int));
-
-    printf("Enter the initial state of the grid (0 for dead, 1 for alive):\n");
-    for (int i = 0; i < n * n; i++) {
-        scanf("%d", &grid[i]);
+    // Create pipes for inter-process communication
+    for (int i = 0; i < NUM_PROCESSES; i++) {
+        if (pipe(pipe_fd[i]) == -1) {
+            fprintf(stderr, "Error creating pipe\n");
+            return 1;
+        }
     }
 
-    int generations;
-    printf("Enter the number of generations to simulate: ");
-    scanf("%d", &generations);
-
-    // Create pipes for communication
-    int **pipes = (int **)malloc(n * sizeof(int *));
-    for (int i = 0; i < n; i++) {
-        pipes[i] = (int *)malloc(2 * sizeof(int));
-        pipe(pipes[i]);
-    }
-
-    // Fork for each row and update generations concurrently
-    for (int gen = 1; gen <= generations; gen++) {
-        for (int row = 0; row < n; row++) {
-            pid_t pid = fork();
-            if (pid == 0) {
-                // Child process
-                close(pipes[row][0]); // Close read end of pipe
-
-                // Update generation for the row
-                updateRow(grid, newGrid, row, n);
-
-                // Write updated row to pipe
-                write(pipes[row][1], newGrid + row * n, n * sizeof(int));
-
-                exit(0);
-            } else if (pid == -1) {
-                // Error in forking
-                printf("Error forking process.\n");
-                exit(1);
+    // Fork child processes and execute Game of Life algorithm
+    for (int i = 0; i < NUM_PROCESSES; i++) {
+        pid[i] = fork();
+        if (pid[i] == 0) {
+            // Child process
+            int start_row = i * rows_per_process;
+            int end_row = (i + 1) * rows_per_process - 1;
+            if (i == NUM_PROCESSES - 1) {
+                // Last process handles any remaining rows
+                end_row = BOARD_SIZE - 1;
             }
+            close(pipe_fd[i][0]); // Close read end of pipe
+            simulate_board(board, start_row, end_row, pipe_fd[i]);
+            close(pipe_fd[i][1]); // Close write end of pipe
+            exit(0);
+        } else if (pid[i] < 0) {
+            fprintf(stderr, "Error forking child process\n");
+            return 1;
         }
-
-        // Wait for all child processes to complete
-        for (int row = 0; row < n; row++) {
-            wait(NULL);
-        }
-
-        // Read updated grid from pipes
-        for (int row = 0; row < n; row++) {
-            read(pipes[row][0], newGrid + row * n, n * sizeof(int));
-        }
-
-        // Copy new grid to original grid for next generation
-        memcpy(grid, newGrid, n * n * sizeof(int));
     }
 
-    // Print the final generation
-    printf("Final generation:\n");
-    printGeneration(grid, n);
+    // Parent process synchronizes with child processes after each generation
+    int msg = 0;
+    for (int gen = 0; gen < NUM_GENERATIONS; gen++) {
+        // Wait for all child processes to complete one iteration
+        for (int i = 0; i < NUM_PROCESSES; i++) {
+            read(pipe_fd[i][0], &msg, sizeof(msg));
+        }
 
-    // Free allocated memory
-    free(grid);
-    free(newGrid);
-    for (int i = 0; i < n; i++) {
-        free(pipes[i]);
+        // Send a message to all child processes to proceed to the next iteration
+        for (int i = 0; i < NUM_PROCESSES; i++) {
+            write(pipe_fd[i][1], &msg, sizeof(msg));
+        }
     }
-    free(pipes);
+
+    // Parent process waits for all child processes to finish
+    for (int i = 0; i < NUM_PROCESSES; i++) {
+        waitpid(pid[i], NULL, 0);
+    }
+
+    // Print the final state of the game board
+    print_board(board);
+
+    // Detach and remove shared memory segment
+    shmdt(board);
+    shmctl(shmid, IPC_RMID, NULL);
 
     return 0;
 }
